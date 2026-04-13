ba1d70b984d4263168ecec3639272dcc6d7e3284
code-game <149863351+CodeGameWeb@users.noreply.github.com>
2026-04-13 10:37:28 +0000

    fix image

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: process.env.NODE_ENV === "production" ? "/Game" : "",
-  trailingSlash: true,
+  basePath: "/Game",
+  assetPrefix: "/Game/",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
