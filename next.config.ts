24ce273a23417ad76c267648affcec6e85aa234e
code-game <149863351+CodeGameWeb@users.noreply.github.com>
2026-04-13 17:10:59 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: process.env.NODE_ENV === "production" ? "/Game" : "",
+  trailingSlash: true,
 };
 
 export default nextConfig;
