f1c1e00cc217c3ee2a367eddbfa9a6f3204709e6
code-game <149863351+CodeGameWeb@users.noreply.github.com>
2026-04-14 10:10:26 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/Game",
-  assetPrefix: "/Game/",
   images: {
     unoptimized: true,
   },
